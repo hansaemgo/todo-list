eb382465acaddc2cecf5af2d36236346527b77d0
hansaemgo <rhgkstoa7@gmail.com>
2020-02-05 11:58:11 +0900

    practice - Todo-List

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import TodoTemplate from './components/TodoTemplate';
-import TodoHead from './components/TodoHead';
-import TodoList from './components/TodoList';
-import TodoCreate from './components/TodoCreate';
-import { TodoProvider } from './TodoContext';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: #e9ecef
-  }
-`;
 function App() {
-  return (
-    <TodoProvider>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
-    </TodoProvider>
-  );
+	return;
 }
 
 export default App;
